a3e96d3fa147c116c6254f8d01fb3dd09c2b9209
Gabriel Duarte <72947594+Duarte64@users.noreply.github.com>
2021-09-17 22:21:56 -0300

    Add files via upload

--- js/hiddenArea.js
+++ js/hiddenArea.js
@@ -2,5 +2,16 @@ const button = document.querySelector(".auxiliar__arrow-img");
 const areaHidden = document.querySelector(".auxiliar__hidden");
 
 button.addEventListener("click", function() {
+    
+    if (button.classList.contains("rotated180")) {
+        button.classList.remove("rotated180");
+        button.classList.add("rotated360");
+    } else if (button.classList.contains("rotated360")){
+        button.classList.remove("rotated360");
+        button.classList.add("rotated180");
+    } else {
+        button.classList.add("rotated180");
+    }
+
     $(".auxiliar__hidden").toggle(500);
 });
